488076bf3b9499b0a2a9222639f7ab6f6949cc8e
moondi <moon-di-2009@yandex.ru>
2021-12-08 18:23:03 +0300

    на экран activity_main.xml 3 кнопки и 3 текстовых поля, описывающих эти кнопки. В MainActivity.kt добавлена реализация логики работы с кнопками: по нажатию на кнопку увеличивается счетчик кнопки на 1 единицу.

--- app/src/main/java/com/example/countersmvp/MainActivity.kt
+++ app/src/main/java/com/example/countersmvp/MainActivity.kt
@@ -2,16 +2,58 @@ package com.example.countersmvp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import com.example.countersmvp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private val mainActivityBinding: ActivityMainBinding? = null
+    private var mainActivityBinding: ActivityMainBinding? = null
+
+    val counters = mutableListOf(0, 0, 0)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        mainActivityBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(mainActivityBinding?.root)
+        mainActivityBinding?.btnCounter1?.setOnClickListener {
+            mainActivityBinding?.btnCounter1?.text = (++counters[0]).toString()
+        }
 
+        mainActivityBinding?.btnCounter2?.setOnClickListener {
+            mainActivityBinding?.btnCounter2?.text = (++counters[1]).toString()
+        }
 
+        mainActivityBinding?.btnCounter3?.setOnClickListener {
+            mainActivityBinding?.btnCounter3?.text = (++counters[2]).toString()
+        }
 
+        initViews()
+
+    }
+
+    private fun initViews() {
+        mainActivityBinding?.btnCounter1?.text = counters[0].toString()
+        mainActivityBinding?.btnCounter2?.text = counters[1].toString()
+        mainActivityBinding?.btnCounter3?.text = counters[2].toString()
+
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putIntArray("counters", counters.toIntArray())
+    }
+
+    override fun onSaveInstanceState(outState: Bundle, outPersistentState: PersistableBundle) {
+        super.onSaveInstanceState(outState, outPersistentState)
+        outState.putIntArray("counters", counters.toIntArray())
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        val countersArray = savedInstanceState.getIntArray("counters")
+        countersArray?.toList()?.let {
+            counters.clear()
+            counters.addAll(it)
+        }
+        initViews()
     }
 }
