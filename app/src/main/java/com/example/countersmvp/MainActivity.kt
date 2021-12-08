70cb8b391bd8704a28d56de48db37dd82bdbc412
moondi <moon-di-2009@yandex.ru>
2021-12-08 14:37:53 +0300

    commit

---
+++ app/src/main/java/com/example/countersmvp/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.countersmvp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.countersmvp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private val mainActivityBinding: ActivityMainBinding? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(mainActivityBinding?.root)
+
+    }
+}
