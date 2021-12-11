bbb8c706ac01c507a9b970cd2d778943724e25e7
moondi <moon-di-2009@yandex.ru>
2021-12-11 12:45:17 +0300

    1. для списка пользователей и элемента пользователя реализованы Адаптер (UsersRVAdapter) и viewHolder

---
+++ app/src/main/java/com/example/countersmvp/view/UsersRVAdapter.kt
@@ -0,0 +1,33 @@
+package com.example.countersmvp.view
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.countersmvp.databinding.TvItemUserBinding
+import com.example.countersmvp.presenter.IUserListPresenter
+import kotlin.coroutines.coroutineContext
+
+class UsersRVAdapter(val presenter: IUserListPresenter):
+    RecyclerView.Adapter<UsersRVAdapter.ViewHolder>(){
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
+        ViewHolder(TvItemUserBinding.inflate(LayoutInflater.from(parent.context),
+        parent, false)).apply {
+            itemView.setOnClickListener {
+                presenter.itemClickListener?.invoke(this)
+            }
+        }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) =
+        presenter.bindView(holder.apply { pos = position })
+
+    override fun getItemCount() = presenter.getCount()
+
+    inner class ViewHolder(val vb: TvItemUserBinding): RecyclerView.ViewHolder(vb.root),
+        UserItemView{
+        override var pos = -1
+        override fun setLogin(text: String) = with(vb) {
+            tvLogin.text = text
+        }
+    }
+}
